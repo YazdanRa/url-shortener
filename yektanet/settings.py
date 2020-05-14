bb90021b38e5cb6bbba657d863e666123797faab
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-05-14 09:50:53 +0430

    - update settings.py

--- yektanet/settings.py
+++ yektanet/settings.py
@@ -1,3 +1,8 @@
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 """
 Django settings for yektanet project.
 
@@ -16,17 +21,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+# Extra model for accounts
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=e=ths%d67rf_nikl$8$7kf&*26k=q)_t5iya(t-f*@$afj^29'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Application definition
 
@@ -37,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My app
+    'accounts.apps.AccountsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -73,12 +73,10 @@ WSGI_APPLICATION = 'yektanet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+try:
+    from .database_sttings import *
+except ImportError:
+    pass
 
 
 # Password validation
@@ -117,4 +115,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'yektanet/static')
+]
+
+
+# Media Folder
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+# Messages
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
+
+
+# Default redirect url
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'index'
